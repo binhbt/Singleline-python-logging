ac740132cdb874d17327e27201882b2ef07bcc51
BinhBT <binhbt@sigma-solutions.eu>
2020-06-26 15:21:09 +0700

    singline for log

--- logging_example.py
+++ logging_example.py
@@ -31,29 +31,46 @@ import sys
 import os
 from logging.handlers import RotatingFileHandler
 
+class OneLineExceptionFormatter(logging.Formatter):
+    def formatException(self, exc_info):
+        result = super().formatException(exc_info)
+        return repr(result)
+ 
+    def format(self, record):
+        result = super().format(record)
+        result = result.replace("\n", "\\n")
+        # if record.exc_text:
+        #     result = result.replace("\n", "")
+        return result
+
+
 logger = logging.getLogger('app')
 logger.setLevel(logging.DEBUG)
 handler = RotatingFileHandler('app.log', maxBytes=512*1024*1024, backupCount=2)
-formatter = logging.Formatter('[%(asctime)s] [p%(process)s] [%(funcName)s] [%(pathname)s:%(lineno)d] [%(levelname)s] - %(message)s','%m-%d %H:%M:%S')
+# formatter = logging.Formatter('[%(asctime)s] [p%(process)s] [%(funcName)s] [%(pathname)s:%(lineno)d] [%(levelname)s] - %(message)s','%m-%d %H:%M:%S')
+formatter = OneLineExceptionFormatter('[%(asctime)s] [p%(process)s] [%(funcName)s] [%(pathname)s:%(lineno)d] [%(levelname)s] - %(message)s','%m-%d %H:%M:%S')
+
 handler.setFormatter(formatter)
 logger.addHandler(handler)
-logger.warning('This is a warning')
-logger.error('This is an error')
-logger.info('This is an info')
-
+# logger.warning('This is a warning')
+# logger.error('This is an error')
+logger.info('This is an info\n\n aaaaaaaaaaaa')
+def trace_log():
+    import traceback
+    just_the_string = traceback.format_exc().replace('\n','!!').replace('\r', '!!!')
+    logger.error(just_the_string)
 def test():
-    logger.info('This is an info test')
+    # logger.info('This is an info test')
     a = 5
     b = 0
     try:
         c = a / b
     except Exception as e:
         # logging.exception("Exception occurred")
-        # logger.error(e)
+        logger.exception(e)
         # logger.exception(str(e))
         # logger.critical(e, exc_info=True)
-        import traceback
-        just_the_string = traceback.format_exc().replace('\n','!!').replace('\r', '!!!')
-        logger.error(just_the_string)
-        logger.error('----------------')
+        # import traceback
+        trace_log()
+
 test()
